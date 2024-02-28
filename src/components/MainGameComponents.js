b35c4c369ccd85d8f29ff945baea72a8d414e17e
think0507 <think0507@naver.com>
2024-02-28 14:17:23 +0900

    modal 활용한 RoomList 화면 구성

--- src/components/MainGameComponents.js
+++ src/components/MainGameComponents.js
@@ -1,6 +1,7 @@
 // MainGameComponents.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import RoomList from "./RoomList";
 
 function MainGameComponents() {
     // URL 파라미터를 가져옵니다.
@@ -16,6 +17,7 @@ function MainGameComponents() {
         <div>
             <h2>{gameName}</h2>
             <p>{gameInfo[gameName]}</p>
+            <RoomList />
         </div>
     );
 }
