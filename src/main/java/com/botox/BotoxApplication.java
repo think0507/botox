2d89a99436034fbba7e25bb3c2800c17953db3ca
baek <think0507@naver.com>
2024-06-27 10:49:48 +0900

    Spring init

---
+++ src/main/java/com/botox/BotoxApplication.java
@@ -0,0 +1,13 @@
+package com.botox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botox/BotoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
