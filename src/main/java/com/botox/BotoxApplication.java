7bcf68dae788bfbeca27a02a8469b90726b98d75
wookd0ng <wook5307@gmail.com>
2024-06-27 10:53:31 +0900

    test branch 생성

--- src/main/java/com/botox/BotoxApplication.java
+++ src/main/java/com/botox/BotoxApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BotoxApplication {
 
+	//시작
 	public static void main(String[] args) {
 		SpringApplication.run(BotoxApplication.class, args);
 	}
