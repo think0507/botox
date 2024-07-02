2c13e37a22148ff7953b218e0a032bcd07ced80c
baek <think0507@naver.com>
2024-07-02 14:15:42 +0900

    Spring init

--- src/main/java/com/botox/BotoxApplication.java
+++ src/main/java/com/botox/BotoxApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BotoxApplication {
-// 브랜치 전략 테스트용 푸쉬
 	public static void main(String[] args) {
 		SpringApplication.run(BotoxApplication.class, args);
 	}
