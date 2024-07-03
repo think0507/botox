5ad9b73b79fee546ac3ac02d6aab0200a32dde66
didwjdals <jlpark0425@naver.com>
2024-07-04 01:08:06 +0900

    도메인 수정

--- src/main/java/com/botox/domain/Chat.java
+++ src/main/java/com/botox/domain/Chat.java
@@ -2,13 +2,16 @@ package com.botox.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter
 @Table(name = "chat")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/botox/domain/Comment.java
+++ src/main/java/com/botox/domain/Comment.java
@@ -1,12 +1,16 @@
 package com.botox.domain;
 
+import com.botox.domain.Post;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
 @Table(name = "comment")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
