3f5b5cd4d852b3e21b37a7ef4567a7022b888f21
wookd0ng <wook5307@gmail.com>
2024-07-04 00:10:23 +0900

    feature_Room 수정1차

--- src/main/java/com/botox/domain/Friendship.java
+++ src/main/java/com/botox/domain/Friendship.java
@@ -1,17 +1,10 @@
 package com.botox.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "friendship")
-@Getter
-@Setter
-@NoArgsConstructor
 public class Friendship {
-    //하나의 엔티티는 단순히 ID로만 저장하고 필요할때 조인하는 방법으로 수정
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,5 +16,6 @@ public class Friendship {
     @Column(name = "requestId")
     private Long requestedUserId;
 
-    // Lombok will generate the getters, setters, and the no-args constructor
+    // Getters, setters, constructors
 }
+
