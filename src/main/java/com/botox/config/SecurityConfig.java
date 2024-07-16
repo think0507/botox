bb127a3f404d23d3e5802a740fe2ef3ff3209fc3
Baek Dongryeol <think0507@naver.com>
2024-07-16 17:25:39 +0900

    SecurityConfig.java 에 허용할 도메인 추가

--- src/main/java/com/botox/config/SecurityConfig.java
+++ src/main/java/com/botox/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 허용할 도메인
+        configuration.setAllowedOrigins(List.of("http://d3ao949apmj1lo.cloudfront.net")); // 허용할 도메인
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
