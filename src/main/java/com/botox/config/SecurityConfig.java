4dc37d14654aa59e9557b03cfb0a3ceee590875f
Baek Dongryeol <think0507@naver.com>
2024-07-19 15:04:40 +0900

    WebConfig.java 수정 -> 도메인 등록

--- src/main/java/com/botox/config/SecurityConfig.java
+++ src/main/java/com/botox/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://d3ao949apmj1lo.cloudfront.net")); // 허용할 도메인
+        configuration.setAllowedOrigins(List.of("https://botox-chat.site")); // 허용할 도메인
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
