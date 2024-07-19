edc5969a3633ddbfa8156206f6d78d43a3cd4372
Baek Dongryeol <think0507@naver.com>
2024-07-19 19:29:24 +0900

    찐막테스트

--- src/main/java/com/botox/config/SecurityConfig.java
+++ src/main/java/com/botox/config/SecurityConfig.java
@@ -52,8 +52,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://d3ao949apmj1lo.cloudfront.net")); // 허용할 도메인
-        configuration.setAllowedOrigins(List.of("https://botox-chat.site")); // 허용할 도메인
+        configuration.setAllowedOrigins(List.of(
+                "http://d3ao949apmj1lo.cloudfront.net",
+                "https://botox-chat.site",
+                "https://www.botox-chat.site")); // 여러 도메인을 한 번에 설정
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
