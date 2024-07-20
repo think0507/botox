1e9873f8b866bc6ef87cf6664c786bd8681b25c3
Baek Dongryeol <think0507@naver.com>
2024-07-20 16:56:25 +0900

    웹소켓 에러 처리 추가 WebSocketConfig.java 에 try-catch 문 작성

--- src/main/java/com/botox/config/WebSocketConfig.java
+++ src/main/java/com/botox/config/WebSocketConfig.java
@@ -1,14 +1,13 @@
 package com.botox.config;
 
-
 import com.botox.controller.ChatWebSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
 import org.kurento.client.KurentoClient;
+import org.kurento.commons.exception.KurentoException;
 
 @Configuration
 @EnableWebSocket
@@ -16,14 +15,19 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Bean
     public KurentoClient kurentoClient() {
-        return KurentoClient.create("ws://localhost:8888/kurento");
+        try {
+            return KurentoClient.create("ws://localhost:8888/kurento");
+        } catch (KurentoException e) {
+            System.err.println("KurentoClient 생성 중 오류 발생: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Bean
     public ChatWebSocketHandler webSocketHandler(KurentoClient kurentoClient) {
         return new ChatWebSocketHandler(kurentoClient);
     }
-
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
