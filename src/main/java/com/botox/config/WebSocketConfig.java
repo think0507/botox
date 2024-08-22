92c89adde0cefbbff73bfc44bf7232cab6579357
asdfg6548 <asdfg6548@syuin.ac.kr>
2024-08-22 09:53:27 +0900

    채팅 수정 중

--- src/main/java/com/botox/config/WebSocketConfig.java
+++ src/main/java/com/botox/config/WebSocketConfig.java
@@ -1,40 +1,3 @@
-//package com.botox.config;
-//
-//import com.botox.controller.ChatWebSocketHandler;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.socket.config.annotation.EnableWebSocket;
-//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-//import org.kurento.client.KurentoClient;
-//import org.kurento.commons.exception.KurentoException;
-//
-//@Configuration
-//@EnableWebSocket
-//public class WebSocketConfig implements WebSocketConfigurer {
-//
-//    @Bean
-//    public KurentoClient kurentoClient() {
-//        try {
-//            return KurentoClient.create("ws://localhost:8888/kurento");
-//        } catch (KurentoException e) {
-//            System.err.println("KurentoClient 생성 중 오류 발생: " + e.getMessage());
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
-//
-//    @Bean
-//    public ChatWebSocketHandler webSocketHandler(KurentoClient kurentoClient) {
-//        return new ChatWebSocketHandler(kurentoClient);
-//    }
-//
-//    @Override
-//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(webSocketHandler(kurentoClient()), "/ws").setAllowedOrigins("http://localhost:3000");
-//    }
-//}
-
 package com.botox.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -53,7 +16,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws") // 연결될 엔드포인트
                 .setAllowedOriginPatterns("*");
     }
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 메시지를 구독(수신)하는 요청 엔드포인트
